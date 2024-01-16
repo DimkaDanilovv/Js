ada032cc2fbb2c350a627b77456807cd208d5979
DimkaDanilovv <DanilovDmitriy769d@gmail.com>
2024-01-16 19:57:28 +0700

    Пятая проверочная

--- js/main.js
+++ js/main.js
@@ -3,23 +3,38 @@ let app = new Vue({
     el: '#app',
     data: {
     product: "Socks",
-    description: "A pair of warm, fuzzy socks.",
     image: "./assets/vmSocks-green-onWhite.jpg",
     altText: "A pair of socks",
     inStock: true,
+    cart: 0,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
 
     variants: [
         {
-        variantId: 2234,
-        variantColor: 'green'
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
         },
         {
-        variantId: 2235,
-        variantColor: 'blue'
+            variantId: 2235,
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
         }
     ],
 
-    sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+    updateProduct(variantImage) {
+        this.image = variantImage
+    }
+    },
+
+    methods: {
+        addToCart() {
+            cart = 0
+            this.cart += 1
+        },
+        deleteToCart() {
+            cart = 0
+            this.cart -= 1
+        }
     },
 })
