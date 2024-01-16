8956e0268db2cad349b92350832c56e72edd6f6a
DimkaDanilovv <DanilovDmitriy769d@gmail.com>
2024-01-16 21:23:49 +0700

    Седьмая проверочная

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ let app = new Vue({
     el: '#app',
     data: {
     product: "Socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
+    brand: 'Vue Mastery',
+    selectedVariant: 0,
     altText: "A pair of socks",
-    inStock: false,
-    cart: 0,
+    onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
 
     variants: [
@@ -14,13 +14,16 @@ let app = new Vue({
             variantId: 2234,
             variantColor: 'green',
             variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            variantQuantity: 10
         },
         {
             variantId: 2235,
             variantColor: 'blue',
             variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            variantQuantity: 0
         }
     ],
+    cart: 0,
 
     },
 
@@ -28,8 +31,27 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         }
     },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            return this.title + " is " + this.onSale;
+            }
+    },
+    
 })
